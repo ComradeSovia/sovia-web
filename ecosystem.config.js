36145c14570f5a5e29bbf3558544942fdfdd44d5
Comrade Sovia <comrade@sovia.work>
2025-12-23 21:22:53 +0100

    Use npm to start

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,8 +2,8 @@ module.exports = {
   apps: [
     {
       name: "sovia-web",
-      script: "pnpm",
-      args: "start",
+      script: "npm",
+      args: "run start",
       env: {
         NODE_ENV: "production",
         PORT: process.env.PORT || 3000,
