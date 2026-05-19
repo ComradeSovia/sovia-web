20ea723bd4adee81a013c9789afeea7dbbdc3f6e
Comrade Sovia <comrade@sovia.work>
2026-05-19 08:13:09 +0200

    Clarify home cards and external actions

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -2,9 +2,14 @@ import type { RouteItem } from "@/definitions/nav.def";
 
 export const Routes = {
   Center: { label: "Center", href: "/" },
-  Sound: { label: "Sound", href: "/sound" },
+  Sound: { label: "Browse Archive", href: "/sound" },
   Report: { label: "Report", href: "/report" },
   Gallery: { label: "Gallery", href: "/gallery" },
+  X: { label: "View Images", href: "https://x.com/ComradeSovia" },
+  Spotify: {
+    label: "Play Originals",
+    href: "https://open.spotify.com/artist/47LCSQdqODH3fx1UvPOHnA?si=EbLMW2NrR26tql5rdfeB9g&pi=eJKQ2GHmQMCOq",
+  },
   Request: { label: "Request", href: "/request" },
   Notice: { label: "Notice", href: "/notice" },
   Youtube: { label: "Youtube", href: "https://www.youtube.com/@ComradeSovia" },
