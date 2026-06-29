41c9c87c27a1135a3a2d7301170f5b1c72291417
Comrade Sovia <comrade@sovia.work>
2026-06-29 15:15:45 +0200

    Update default site URL from https://sovia.app to https://sovia.work

--- src/shared/config/site.ts
+++ src/shared/config/site.ts
@@ -9,7 +9,7 @@ export const SITE_DESCRIPTION = copy.site.description;
 const rawSiteUrl =
   process.env.NEXT_PUBLIC_SITE_URL ||
   process.env.SITE_URL ||
-  "https://sovia.app";
+  "https://sovia.work";
 
 export const SITE_URL = rawSiteUrl.replace(/\/$/, "");
 
