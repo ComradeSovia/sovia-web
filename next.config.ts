5d9b87aae4413545db8d443a1fbeb7ff319eb7b7
Comrade Sovia <comrade@sovia.work>
2026-06-29 22:03:06 +0200

    Add sharp dependency and configure outputFileTracingIncludes in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,13 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   output: "standalone",
+  outputFileTracingIncludes: {
+    "/*": [
+      "./node_modules/sharp/**/*",
+      "./node_modules/@img/sharp*/**/*",
+      "./node_modules/.pnpm/@img+sharp*/**/*",
+    ],
+  },
 };
 
 export default nextConfig;
